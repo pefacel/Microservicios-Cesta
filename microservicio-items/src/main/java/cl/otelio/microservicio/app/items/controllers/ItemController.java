28ee480eb9a7f1fda3ab0fbbd08976340a88d853
Pefacel <pefacel@gmail.com>
2021-04-22 15:57:23 -0400

    Configuraciones remotas desde GitHub

--- microservicio-items/src/main/java/cl/otelio/microservicio/app/items/controllers/ItemController.java
+++ microservicio-items/src/main/java/cl/otelio/microservicio/app/items/controllers/ItemController.java
@@ -1,8 +1,17 @@
 package cl.otelio.microservicio.app.items.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +23,23 @@ import cl.otelio.microservicio.app.items.models.Producto;
 import cl.otelio.microservicio.app.items.service.ItemServiceFeign;
 
 @RestController
+@RefreshScope
 public class ItemController {
 
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	private ItemServiceFeign itemService;
+	
+	
+	@Value("${configuracion.texto}")
+	private String texto;
+	
+	@Value("${server.port}")
+	private String port;
 
 	@GetMapping("/listar")
 	public List<Item> listar() {
@@ -39,6 +61,22 @@ public class ItemController {
 		producto.setNombre("Error al buscar el producto solicitado. Estamos trabajando para usted");
 		item.setProducto(producto);
 		return item;
+	}
+	
+	@GetMapping("/config")
+	public ResponseEntity<?> obtenerConfig(){
+		log.info(texto);
+		
+		Map<String,String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("port", port);
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+
+		return new ResponseEntity<Map<String,String>>(json, HttpStatus.OK);
 	}
 
 }
